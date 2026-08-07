b8974f0069d01010d1e6a4efbafb48c38903ab5d
victoria-vc <vcastagno@frba.utn.edu.ar>
2026-08-07 01:02:28 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,58 @@
+## -------------- Video 01 del curso: "Primer programa en Python" -------------- ##
+print("Hello World!")
+## -------------- Video 01 del curso: "Primer programa en Python" -------------- ##
+
+
+## ------------------------ Video 02 del curso: "Tipos" ------------------------ ##
+# 12 : int
+# 12.4 : float
+# "Tomas 18" : str
+# True : bool
+# False : bool
+
+type(12)
+type(12.4)
+type("Tomas 18")
+type(True)
+type(False)
+
+# Cambiar el tipo de expresión: typecasting
+float(2) # => 2.0
+int(4.4) # => 4
+int('1') # => 1
+int('A') # => ValueError: invalid literal for int() with base 10: 'A'
+str(1) # => '1'
+str(4.4) # => '4.4'
+int(True) # => 1
+int(False) # => 0
+bool(1) # => True
+bool(0) # => False
+## ------------------------ Video 02 del curso: "Tipos" ------------------------ ##
+
+
+## ---------------- Video 03 del curso: "Expresiones y Variables" -------------- ##
+# Expresiones
+# 40 + 20 = 60
+# 40 y 20 son operandos (operands)
+# los símbolos matemáticos, en este caso la suma, se llaman operadores (operators)
+10 + 10
+20 - 10
+10 * 2
+20 / 2
+20 // 3 # => con la doble barra (//) el resultado se redondea hacia abajo
+
+# Variables
+variable = 10 # le asignamos un valor
+variable = 10 + 10 # le asignamos el resultado de una expresión
+variable2 = variable / 3 # le asignamos el resultado de variable / 3
+type(variable) # => int
+type(variable2) # => float
+
+## ---------------- Video 03 del curso: "Expresiones y Variables" -------------- ##
+
+
+## ---------------- Video 04 del curso: "Operaciones con Strings" -------------- ##
+
+
+
+## ---------------- Video 04 del curso: "Operaciones con Strings" -------------- ##
